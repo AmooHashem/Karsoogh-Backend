333857949bae7c573c562e0ba2500a8b4312617a
Sepehr Shirani <sepishiran@gmail.com>
2021-03-25 19:29:27 +0430

    edit is_pass in views.py

---
+++ Api/management/commands/make_exam_student.py
@@ -0,0 +1,19 @@
+from django.core.management import BaseCommand
+
+from Api.models import ExamStudent, Exam, Student
+import os
+
+
+class Command(BaseCommand):
+    help = 'create ExamStudent by exam and student name'
+
+    def handle(self, *args, **options):
+        query_exam = list(Exam.objects.all())
+        query_student = list(Student.objects.all())
+        for exam in query_exam:
+            for student in query_student:
+                ExamStudent.objects.create(
+                    exam=exam,
+                    student=student,
+                )
+                print('ExamStudent created')
