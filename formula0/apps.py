157096996f0cad166383a2d75f9c03f4ee2a581a
amoohashem <amoo.hashem.mehraban@gmail.com>
2021-04-04 23:35:41 +0430

    rename app

---
+++ formula0/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Formula0Config(AppConfig):
+    name = 'formula0'
--- formula3/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Formula3Config(AppConfig):
-    name = 'formula3'
